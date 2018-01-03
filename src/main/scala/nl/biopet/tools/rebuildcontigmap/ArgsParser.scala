b29f71a85c82224929cde5877481477dca010b2d
DavyCats <davycats.dc@gmail.com>
2018-01-03 15:45:31 +0100

    remove unbounded

--- src/main/scala/nl/biopet/tools/rebuildcontigmap/ArgsParser.scala
+++ src/main/scala/nl/biopet/tools/rebuildcontigmap/ArgsParser.scala
@@ -27,15 +27,15 @@ import nl.biopet.utils.tool.{AbstractOptParser, ToolCommand}
 
 class ArgsParser(toolCommand: ToolCommand[Args])
     extends AbstractOptParser[Args](toolCommand) {
-  opt[File]('I', "inputContigMap") required () unbounded () valueName "<file>" action {
+  opt[File]('I', "inputContigMap") required () valueName "<file>" action {
     (x, c) =>
       c.copy(inputContigMap = x)
   } text "Input contig map"
-  opt[File]('o', "outputContigMap") required () unbounded () valueName "<file>" action {
+  opt[File]('o', "outputContigMap") required () valueName "<file>" action {
     (x, c) =>
       c.copy(outputContigMap = x)
   } text "output contig map"
-  opt[File]('R', "referenceFasta") required () unbounded () valueName "<file>" action {
+  opt[File]('R', "referenceFasta") required () valueName "<file>" action {
     (x, c) =>
       c.copy(referenceFasta = x)
   } text "Reference fasta file"
